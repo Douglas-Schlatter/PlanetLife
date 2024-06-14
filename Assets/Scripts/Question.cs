246ff68df3352820b88be133453c18d43dfe7f3b
Douglas-Schlatter <douglas.ardenghi18@gmail.com>
2024-06-13 23:38:32 -0300

    Base Of The Project

---
+++ Assets/Scripts/Question.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Question
+{
+    // Start is called before the first frame update
+    //Para implementar os multiplos scores dos diferentes tipos de jogador, seria bom trocar o inst score por um vetor, que tde acordo
+    //com a aposição seráo score afetado pela pergunta
+    //depois é so intanciar pelo gamecontroller
+    public int id;
+    public string question = "";
+    //nas respostas depois tenho que armazenar quantos recursos precisa e se escolhela da algum efeito positivo ou negativo
+    public string answ1 = "";
+   // public int answ1Value = 0;
+    //public int[] answ1scores = { 0, 0, 0, 0, 0 };
+    public string answ2 = "";
+    //public int answ2Value = 0;
+    //public int[] answ2scores = { 0, 0, 0, 0, 0 };
+    public string answ3 = "";
+    //public int answ3Value = 0;
+    //public int[] answ3scores = { 0, 0, 0, 0, 0 };
+    public string answ4 = "";
+    //public int answ4Value = 0;
+    //public int[] answ4scores = { 0, 0, 0, 0, 0 };
+    // public int[,] answScores = { { 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0 } };
+    public int[][] answScores = new int[4][]{
+    new int[] {0, 0, 0, 0,0},// answ1
+    new int[] {0, 0, 0, 0,0},// answ2
+    new int[] {0, 0, 0, 0,0},// answ3
+    new int[] {0, 0, 0, 0,0}// answ4
+    };
+    public int[] minimumScore = { 0, 0, 0, 0, 0 };
+    public int[] maximumScore = { 0, 0, 0, 0, 0 };
+
+   
+    public Question() 
+    { 
+         
+    }
+    // tem um jeito melhor de fazer isso,sendo ou criando isso junto de um new ou com labda
+    // a versão com new teria de re organizar de modo que todo new dessa classe ja teria de ser oferecido
+    //juntos e logo depois fazer o maximumScore a partir desses argumentos
+    //com labda poderia atualizar automatico dado uma fuinção, mas teria de pesquisar mais
+    //acho que seria com logica de pipeline .net em c
+    //essa forma não é tão robusta, mas funciona
+    public void AttMaxMinScore() 
+    {
+        int maxMoment = 0;
+        int minMoment = 0;
+        for (int i = 0; i < 5; i++)
+        {
+            foreach (int[] j in answScores)
+            {
+                if (maxMoment < j[i])
+                {
+                    maxMoment = j[i];
+                }
+            }
+            maximumScore[i] = maxMoment;
+            maxMoment = 0;
+        }
+        for (int i = 0; i < 5; i++)
+        {
+            foreach (int[] j in answScores)
+            {
+                if (minMoment > j[i])
+                {
+                    minMoment = j[i];
+                }
+            }
+            minimumScore[i] = minMoment;
+            minMoment = 0;
+        }
+    }
+
+}
